b9d16ad227c326869bad9834edb3cfc71ca986ad
Alex Ferreira <alex@dsol.com.br>
2015-09-16 19:25:26 -0300

    fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-
+var mergeTrees = require( 'broccoli-merge-trees' );
 module.exports = {
   name: 'ember-cli-admin',
 
   included: function(app) {
-    this.app = app;
-    app.import(app.bowerDirectory + '/bootstrap-sass-official/assets/javascripts/bootstrap.js');
-    app.import('vendor/fileicon.png', { destDir: 'assets/images' });
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+    app.import('vendor/fileicon.png', {
+      destDir: 'assets/images'
+    });
     app.import(app.bowerDirectory + '/typeahead.js/dist/bloodhound.min.js');
     app.import(app.bowerDirectory + '/typeahead.js/dist/typeahead.jquery.js');
     app.import(app.bowerDirectory + '/jquery-ui-sortable/jquery-ui-sortable.js');
@@ -15,13 +17,22 @@ module.exports = {
     app.import('vendor/nested-sortable.js');
   },
 
-  treeForPublic: function() {
-    var bootstrapFonts = path.join(this.app.bowerDirectory, '/bootstrap-sass-official/assets/fonts/bootstrap');
+  /*treeForPublic: function() {
+    var bootstrapFonts = path.join(this.app.bowerDirectory, '/bootstrap/dist/fonts');
     var bootstrapFontsTree = new Funnel(this.treeGenerator(bootstrapFonts), {
       srcDir: '/',
-      destDir: '/fonts/bootstrap'
+      destDir: '/fonts'
     });
 
     return bootstrapFontsTree;
+  },*/
+  postprocessTree: function(type, tree) {
+    var bootstrapFonts = path.join(this.app.bowerDirectory, '/bootstrap/dist/fonts');
+    var bootstrapFontsTree = new Funnel(this.treeGenerator(bootstrapFonts), {
+      srcDir: '/',
+      destDir: '/fonts'
+    });
+
+    return mergeTrees([tree, bootstrapFontsTree], { overwrite: true });
   }
 };
